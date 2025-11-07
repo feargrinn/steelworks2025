2abe094fd7bd6f29732c3ed96b55aa087a79f8f0
Liero <lierotoma@gmail.com>
2025-11-07 23:40:32 +0100

    person station interaction

--- GameStation/game_station.gd
+++ GameStation/game_station.gd
@@ -1,15 +1,20 @@
 extends Area2D
 
+class_name GameStation
+
 @export var station_id: int = 0 # Maybe unnecessary
 @export var retrieval_time: float = 2.0 # How much time between gambles for tickets
+const error_station_distance: float = 20.0
 
 @onready var highlight_sprite = $Highlight
 @onready var required_position: Node2D = $PlayerPosition
+@onready var go_away_position: Node2D = $GoAwayPosition
 
 var reward_retrieval_timer: float = 0.0 # How much time left for the next gamble
-var assigned_person: Node2D = null
-var player_person: Node2D = null
+var assigned_person: Person = null
+var player_person: Person = null
 var highlighted: bool = false
+var waiting_for_player: bool = false
 
 func _ready() -> void:
 	highlight_sprite.hide()
@@ -17,25 +22,42 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	# Clicked on the station
 	if Input.is_action_just_pressed("left_click") and highlighted:
-		print("Clicked machine with id: ", station_id)
+		print("Clicked station with id: ", station_id)
 		
 		# If there is a person selected we make him go towards the station (he is assigned)
 		if game_manager.currently_selected_person != null:
+			if assigned_person:
+				assigned_person.set_target_position(assigned_person.global_position) # former assigned stops
+				assigned_person = null
+			if player_person: 
+				player_person.set_target_position(go_away_position.global_position) # Former player goes away
+				player_person = null
+			
+			# new assigned goes towards station
+			waiting_for_player = true
 			assigned_person = game_manager.currently_selected_person
-			# TODO: make assigned person go towards required_position
+			assigned_person.set_target_position(required_position.global_position)
+			print("Person ", assigned_person.id ," goes towards station with id: ", station_id)
 	
 	# Checking if the person has finished walking to the station
 	if assigned_person != null:
-		if assigned_person.global_position.distance_to(required_position.global_position):
-			# TODO: make the former assigned_person go away
+		if waiting_for_player and assigned_person.global_position.distance_to(required_position.global_position) <= error_station_distance:
+			# Assign new player
+			waiting_for_player = false
 			player_person = assigned_person
+			assigned_person = null
 			reward_retrieval_timer = retrieval_time # Reset the time for getting a new reward
+			print("Person ", player_person.id ," started playing station with id: ", station_id)
 			
-	if player_person != null:
+	if player_person and player_person.global_position.distance_to(required_position.global_position) > error_station_distance:
+		player_person = null
+	
+	# gambling for tickets
+	if player_person != null and !waiting_for_player:
 		if reward_retrieval_timer <= 0.0:
 			reward_retrieval_timer = retrieval_time
-			# TODO: gamble for tickets
-			
+			game_manager.no_collected_tickets += 1
+			print("Tickets: ", game_manager.no_collected_tickets)
 		reward_retrieval_timer -= delta
 
 
--- person/person.gd
+++ person/person.gd
@@ -2,11 +2,18 @@ extends CharacterBody2D
 
 class_name Person
 
+@export var id: int = 0 # Maybe unnecessary
 @export var speed: float = 100
 
 @onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 
+var highlighted: bool = false
+
 func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("left_click") and highlighted:
+		print("Clicked person with id: ", id)
+		game_manager.currently_selected_person = self
+	
 	if not navigation_agent_2d.is_navigation_finished():
 		var dir := to_local(navigation_agent_2d.get_next_path_position()).normalized()
 		velocity = dir * speed
@@ -15,3 +22,11 @@ func _physics_process(_delta: float) -> void:
 
 func set_target_position(target_position: Vector2) -> void:
 	navigation_agent_2d.target_position = target_position
+
+
+func _on_mouse_click_detection_mouse_entered() -> void:
+	highlighted = true
+	# TODO: make a highlight
+
+func _on_mouse_click_detection_mouse_exited() -> void:
+	highlighted = false
