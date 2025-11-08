dea9bf66587d12320ce26e1e0c787ebbe19eb2d3
jGrzyb <grzybjaku@gmail.com>
2025-11-08 22:39:17 +0100

    dfx + ghost death

--- UI/audio_manager.gd
+++ UI/audio_manager.gd
@@ -1,9 +1,23 @@
 extends Node
 
+@onready var sfx: Node = $sfx
+var audio_players: Array[AudioStreamPlayer]
+
 var music_value = 0.5
 var sfx_value = 0.5
 
+
+func _ready() -> void:
+	for n in sfx.get_children():
+		if n is AudioStreamPlayer:
+			audio_players.append(n)
+
+
 func play_music():
 	$Music.play()
-func play_sfx():
-	$SFX.play()
+
+
+func play_sfx(sound_name: String):
+	var idx: int = audio_players.find_custom(func(node: Node): return node.name == sound_name)
+	assert(idx != -1, "No sound with name: %s" % sound_name)
+	audio_players[idx].play()
