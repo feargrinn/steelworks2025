c9121f9a3ab6f0edd4082de23f6f044609e6e56b
Liero <lierotoma@gmail.com>
2025-11-09 12:49:41 +0100

    ghost animation

--- ghost/ghost.gd
+++ ghost/ghost.gd
@@ -4,11 +4,19 @@ extends Area2D
 @export var SPEED: int = 100
 
 @onready var node_to_follow: Node2D = get_tree().get_nodes_in_group("person").pick_random()
-
+@onready var sprite_animation: AnimatedSprite2D = $AnimatedSprite2D
+@onready var fade_animation: AnimationPlayer = $AnimationPlayer
+var dead: bool = false
 
 func _physics_process(delta: float) -> void:
+	if dead: return
+	
 	var goal = node_to_follow.global_position if node_to_follow else get_viewport_rect().get_center()
 	var direction := (goal - global_position).normalized()
+	
+	if direction.x > 0.0: sprite_animation.flip_h = true
+	else: sprite_animation.flip_h = false
+	
 	if (goal - global_position).length_squared() < pow(SPEED * delta, 2):
 		global_position = goal
 		return
@@ -18,5 +26,11 @@ func _physics_process(delta: float) -> void:
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			dead = true
 			AudioManager.play_sfx("ghost_death")
-			queue_free()
+			sprite_animation.animation = "death"
+			fade_animation.current_animation = "death"
+			
+
+func _dispose(): # Called from death animation
+	queue_free()
